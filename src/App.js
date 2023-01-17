a99fbdf31628aec8bf9679eb3a961c5ef20c987c
Razi <12abdulrahim21@gmail.com>
2023-01-18 00:57:17 +0530

    Post category done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import AddNewCategory from "./components/Categories/AddNewCategory";
+import HomePage from "./components/HomePage/HomePage";
+import Navbar from "./components/Navigation/Navbar";
+
+
+import Login from "./components/Users/Login/Login";
+import Register from "./components/Users/Register/Register";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar/>
+        
+      
+        <Routes>
+          <Route exact path = '/add-category' element={<AddNewCategory/>}/>
+          <Route exact path = '/' element={<HomePage />}/>
+          <Route exact path = '/register' element={<Register/>}/>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
---
+++ src/redux/store/store.js
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit'
+import usersReducer from "../slices/users/usersSlices";
+import categoriesReducer from "../slices/category/categorySlice"
+
+
+const store = configureStore({
+    reducer: {
+        users: usersReducer,
+        category:categoriesReducer
+        
+},
+})
+
+export default store;
