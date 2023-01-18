cc2fef413c60f51957eea924c6e8b12d2910f532
Razi <12abdulrahim21@gmail.com>
2023-01-18 18:08:38 +0530

    post slice reducer created

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit'
 import usersReducer from "../slices/users/usersSlices";
 import categoriesReducer from "../slices/category/categorySlice"
+import post from "../slices/posts/postSlices"
 
 
 const store = configureStore({
     reducer: {
         users: usersReducer,
-        category:categoriesReducer
+        category:categoriesReducer,
+        post
         
 },
 })
