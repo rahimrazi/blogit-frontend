3db4cd45318605527f507c5d58d64df897c773e7
Razi <12abdulrahim21@gmail.com>
2023-01-18 17:29:27 +0530

    post component created

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Register from "./components/Users/Register/Register";
 import CategoryList from "./components/Categories/CategoryList";
 import UpdateCategory from "./components/Categories/UpdateCategory";
 
+import AdminProtectedRoute from "./components/Navigation/ProtectedRoutes/AdminProtectedRoute";
+import CreatePost from "./components/Posts/CreatePost";
+import UserProtectedRoute from "./components/Navigation/ProtectedRoutes/UserProtectedRoute";
+
+
 function App() {
   return (
     <div>
@@ -15,12 +20,32 @@ function App() {
         <Navbar />
 
         <Routes>
+          
+          <Route element={<AdminProtectedRoute />}>
+            {/* <Route path="/admin-dashboard" element={<AdminDashboard />} /> */}
+            {/* <Route path="/send-mail" element={<SendEmail />} /> */}
+
+            {/* <Route path="/users" element={<UsersList />} /> */}
+            {/* <Route path="/reported-list" element={<ReportedPost />} /> */}
+
+            <Route path="/add-category" element={<AddNewCategory />} />
+
+            <Route path="/update-category/:id" element={<UpdateCategory />} />
+          </Route>
+          {/* <Route exact path="/add-category" element={<AddNewCategory />} /> */}
+
           <Route
-            exact
-            path="/update-category/:id"
-            element={<UpdateCategory />}
+            path="/create-post"
+            element={
+              <UserProtectedRoute>
+                <CreatePost />
+              </UserProtectedRoute>
+            }
           />
-          <Route exact path="/add-category" element={<AddNewCategory />} />
+          
+          
+          
+          
           <Route exact path="/category-list" element={<CategoryList />} />
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/register" element={<Register />} />
