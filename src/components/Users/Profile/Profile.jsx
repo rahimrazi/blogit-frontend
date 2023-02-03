7a4c095ecf0101abd6b7c65f350c8c7d6e03a08d
Razi <12abdulrahim21@gmail.com>
2023-02-03 15:03:18 +0530

    front end changes made for displaying account type(starter or pro)

--- src/components/Users/Profile/Profile.jsx
+++ src/components/Users/Profile/Profile.jsx
@@ -81,7 +81,7 @@ export default function Profile() {
                               <h1 className="text-2xl font-bold text-gray-900 ">
                                 {profile?.firstName} {profile?.lastName}
                                 <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-yellow-100 text-yellow-800">
-                                  {/* {profile?.accountType} */}
+                                  {profile?.accountType}
                                 </span>
                                 {/* Display if verified or not */}
                                 {profile?.isAccountVerified ? (
