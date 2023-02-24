758962ee5e764f55c4f9dd045adfa189bf9b22cd
Razi <12abdulrahim21@gmail.com>
2023-02-25 00:44:11 +0530

    reinstalled

--- src/components/Chat/SingleChat.jsx
+++ src/components/Chat/SingleChat.jsx
@@ -23,7 +23,7 @@ import animationData from "../../animations/typing.json";
 
 import io from "socket.io-client";
 import Lottie from "react-lottie";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://pizahub.online";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
